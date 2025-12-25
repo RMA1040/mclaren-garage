5881bab4fde0eb83970fea59aca7cab5cc4e3294
Raymond Deruelle <ray@Raymonds-MacBook-Air.local>
2025-12-25 19:32:44 +0100

    Add public and views folders

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ app.set("views", path.join(__dirname, "../src/views"));
 
 
 // ===== Static files =====
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "../public")));
 
 
 // ===== Routes =====
